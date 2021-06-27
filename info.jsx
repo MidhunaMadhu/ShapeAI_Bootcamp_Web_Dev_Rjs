a67bad803484291c021b72325f3bfd4bc1880a98
MidhunaMadhu <86552454+MidhunaMadhu@users.noreply.github.com>
2021-06-27 02:45:38 -0700

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="container">
+        <div className="note">
+          <h1> Javascript and React.js</h1>
+          <br /> <br />
+          <p>
+            7 Days Free Bootcamp on Basic Web Dev in collaboration with Google
+            Developer Groups, Microsoft Learn Student Ambassador Program, and
+            AWS Students Club
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;54.1k like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;5.4k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="note">
+          <h1> Python and NLP Bootcamp</h1>
+          <br /> <br />
+          <p>
+            Now we will be starting with the final registration for the 7 days
+            Bootcamp ONLY THE FINAL REGISTERED STUDENTS WILL BE ELIGIBLE FOR THE
+            CERTIFICATES ON COMPLETION OF THE BOOTCAMP
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;44 like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;9.5k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="note">
+          <h1> Python & Networking Security </h1>
+          <p>
+            Now we will be starting with the final registration for the 7 days
+            Bootcamp ONLY THE FINAL REGISTERED STUDENTS WILL BE ELIGIBLE FOR THE
+            CERTIFICATES ON COMPLETION OF THE BOOTCAMP
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;444 like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;9.1k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="note">
+          <h1> HTML & CSS Javascript Bootcamp</h1>
+
+          <br />
+          <p>
+            HTML(the Hypertext Markup Language) and CSS(Cascading Style Sheets)
+            are two of the core technologies for building Webpages.HTML provides
+            the structure of the page CSS.
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;4.4k like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;9.4k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="note">
+          <h1>Backend Web Dev with Node.Js </h1>
+          <p>
+            Now we will be starting with the final registration for the 7 days
+            Bootcamp ONLY THE FINAL REGISTERED STUDENTS WILL BE ELIGIBLE FOR THE
+            CERTIFICATES ON COMPLETION OF THE BOOTCAMP
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;4.3 like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;5.44k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="note">
+          <h1> Python a& Computer Vision</h1>
+          <p>
+            Now we will be starting with the final registration for the 7 days
+            Bootcamp ONLY THE FINAL REGISTERED STUDENTS WILL BE ELIGIBLE FOR THE
+            CERTIFICATES ON COMPLETION OF THE BOOTCAMP
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;4.2 like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;9.56k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="note">
+          <h1> Python & Data Visualization </h1>
+          <p>
+            Now we will be starting with the final registration for the 7 days
+            Bootcamp ONLY THE FINAL REGISTERED STUDENTS WILL BE ELIGIBLE FOR THE
+            CERTIFICATES ON COMPLETION OF THE BOOTCAMP
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;4.1k like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;9.45k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="note">
+          <h1>Tableau Fundamentals Bootcamp</h1>
+
+          <p>
+            Now we will be starting with the final registration for the 7 days
+            Bootcamp ONLY THE FINAL REGISTERED STUDENTS WILL BE ELIGIBLE FOR THE
+            CERTIFICATES ON COMPLETION OF THE BOOTCAMP
+          </p>
+          <div className="icon">
+            <div className="like">
+              <i class="bi bi-hand-thumbs-up-fill"></i>&nbsp;4.44k like
+            </div>
+            <div className="like">
+              &nbsp;<i class="bi bi-eye-fill"></i>&nbsp;10.4k
+            </div>
+            <div>
+              <a href="#">
+                <button type="button" name="" className="btn">
+                  Register
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Info;
